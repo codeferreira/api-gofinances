26c4c0d0c7f19c0ffb437fee54e64e7f3d02f6ee
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2020-04-23 13:06:29 -0300

    feat: Add function to Tranasactions Repository

--- backend/src/repositories/TransactionsRepository.ts
+++ backend/src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,35 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((total, transaction) => {
+      return transaction.type === 'income' ? total + transaction.value : total;
+    }, 0);
+
+    const outcome = this.transactions.reduce((total, transaction) => {
+      return transaction.type === 'outcome' ? total + transaction.value : total;
+    }, 0);
+
+    const total = income - outcome;
+
+    const balance = {
+      income,
+      outcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
